0c0e88c60168eb967db076a6f6351bc5b410251d
Your Name <you@example.com>
2025-04-21 21:08:21 +0200

    falta conectar wp con nginx para ver si furula y creo que ya

---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f "/usr/local/bin/wp" ]; then
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+fi
+
+while [ ! -f "/wordpress/wp-config.php" ]; do
+	if wp core download --path=/wordpress 2>/dev/null; then
+		break
+	fi
+done
+
+if [ ! -f "/wordpress/wp-config.php" ]; then
+	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST
+	wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_email=$WP_ADMIN_EMAIL
+fi
+
+tail -f /dev/null
