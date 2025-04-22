405ac3b04149f714591fca276c0b4bd5435fcc3d
Your Name <you@example.com>
2025-04-22 13:08:00 +0200

    no reescribe el entrypoint de wp :c

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -15,6 +15,13 @@ if [ ! -f "/wordpress/wp-config.php" ]; then
 	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST
 	wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_email=$WP_ADMIN_EMAIL
 	wp user create $WP_USER $WP_EMAIL --role=administrator --user_pass=$WP_PASS
+	wp theme install twentysixteen
+	wp theme install astra
+	wp theme install oceanwp
+	wp theme install twentytwenty  --activate
+	wp theme install variations
+	wp theme install phlox
+	wp theme install spexo
 
 fi
 
