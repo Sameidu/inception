8c67624d3c681b2f70982b14342c5d772d6ca6fa
Your Name <you@example.com>
2025-04-24 21:21:04 +0200

    2/5 uwu

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -15,13 +15,15 @@ if [ ! -f "/wordpress/wp-config.php" ]; then
 	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST
 	wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_email=$WP_ADMIN_EMAIL
 	wp user create $WP_USER $WP_EMAIL --role=administrator --user_pass=$WP_PASS
-	wp theme install twentysixteen
 	wp theme install astra
 	wp theme install oceanwp
 	wp theme install twentytwenty  --activate
 	wp theme install variations
-	wp theme install phlox
 	wp theme install spexo
+	sed -i "93idefine( 'WP_REDIS_HOST', 'redis' );" /wordpress/wp-config.php
+	sed -i "94idefine( 'WP_REDIS_PORT', 6379 );" /wordpress/wp-config.php 
+	wp plugin install redis-cache --activate
+	wp redis enable
 
 fi
 
